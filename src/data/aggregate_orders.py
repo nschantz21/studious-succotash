0aaceb78a4b606c733bd51f42d88d981509ac03f
nschantz21 <nschantz21@gmail.com>
2021-12-14 22:23:53 +0000

    updated methods

--- src/data/aggregate_orders.py
+++ src/data/aggregate_orders.py
@@ -1,6 +1,7 @@
 # imports
 import pandas as pd
 import numpy as np
+from collections import defaultdict
 
 # constants
 output_fields = [
@@ -20,7 +21,27 @@ orders = pd.read_csv(input_fp)
 # create empty orderbook
 orderbook = pd.DataFrame(columns=output_fields)
 
+helper_fields = ["timestamp", "side", "id", "quantity"]
+buy_frame = pd.DataFrame(columns=helper_fields).set_index(helper_fields[:-1])
+sell_frame = pd.DataFrame(columns=helper_fields).set_index(helper_fields[:-1])
+
 # iterate through the orders
 for index, row in orders.iterrows():
-    pass
+    side = row["side"]
+    action = row["action"]
+    print(row)
 
+    rw = row.to_frame().set_index(helper_fields[:-1])
+    if side == 'b':
+        ob_helper = buy_frame
+    else:
+        ob_helper = sell_frame
+    
+    if action == "b":
+        ob_helper.loc[rw.index] += rw["quantity"]
+    if action == "d":
+        ob_helper[hash("".join(row[["timestamp", "side", "id"]].values))] -= row["quantity"]
+    else:
+        ob_helper[hash(row[["timestamp", "side", "id"]].values)] = row["quantity"]
+    
+    print(ob_helper)
