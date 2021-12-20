80e62a1b4c284037d767b8017a3d58f1c4e56331
nschantz21 <nschantz21@gmail.com>
2021-12-20 18:41:15 +0000

    updated makefile and created feature builder

--- Makefile
+++ Makefile
@@ -1,22 +1,31 @@
-# Bash file for easy data creation
-unzip data/external/3rqtest.zip -x *.docx -d data/raw/
+unzip: 
+	unzip data/external/3rqtest.zip -x *.docx -d data/raw/
 
-python -m src.data.make_orderbook \
-data/raw/codetest/res_20190610.csv \
-data/interim/20190610.csv
+orderbooks: 
+	python -m src.data.make_orderbook \
+    data/raw/codetest/res_20190610.csv \
+    data/interim/20190610.csv;
 
-python -m src.data.make_orderbook \
-data/raw/codetest/res_20190611.csv \
-data/interim/20190611.csv
+	python -m src.data.make_orderbook \
+    data/raw/codetest/res_20190611.csv \
+    data/interim/20190611.csv;
+	
+	python -m src.data.make_orderbook \
+    data/raw/codetest/res_20190612.csv \
+    data/interim/20190612.csv
+	
+	python -m src.data.make_orderbook \
+    data/raw/codetest/res_20190613.csv \
+    data/interim/20190613.csv
+	
+	python -m src.data.make_orderbook \
+    data/raw/codetest/res_20190614.csv \
+    data/interim/20190614.csv
+    
+features:
+    #echo "Making Features"
+	python -m src.features.build_features
+    # I would make this more generalized with more time
 
-python -m src.data.make_orderbook \
-data/raw/codetest/res_20190612.csv \
-data/interim/20190612.csv
-
-python -m src.data.make_orderbook \
-data/raw/codetest/res_20190613.csv \
-data/interim/20190613.csv
-
-python -m src.data.make_orderbook \
-data/raw/codetest/res_20190614.csv \
-data/interim/20190614.csv
+#model:
+    #echo "Making the Model"
---
+++ src/features/build_features.py
@@ -0,0 +1,46 @@
+# imports
+import pandas as pd
+import random
+
+# constants
+input_file_name = "20190612"
+input_data_fp = "data/interim/{}.csv".format(input_file_name)
+
+p = 0.1  # percent of lines in file to sample
+random.seed(42)
+liquidity_steps_forward = 5000
+
+# import sample of data
+df = pd.read_csv(
+         input_data_fp,
+         header=0, 
+         skiprows=lambda i: i>0 and random.random() > p)
+
+df.drop_duplicates(subset="timestamp", inplace=True)
+
+# calculate the bid-ask spreads and balance of quantities at each level
+for x in range(5):
+    df["spread{}".format(x)] = df["ap{}".format(x)] - df["bp{}".format(x)]
+    # spread balance: positive is excess demand, negative is excess supply
+    df["spread_balance_{}".format(x)] = df["bq{}".format(x)] - df["aq{}".format(x)]
+
+momentum = df["price"].diff(liquidity_steps_forward).rename("momentum")
+balances = df.filter(regex="spread_balance_0").diff(liquidity_steps_forward).shift(-liquidity_steps_forward).iloc[:,0].rename("fwd_liq")
+
+other_balances = df.filter(regex="spread_balance_[0-4]").diff(liquidity_steps_forward)
+
+# volatility
+price_volatility = df["price"].rolling(liquidity_steps_forward).std().rename("price_volatility")
+
+
+liquidity_features = pd.concat(
+    [
+        balances,
+        other_balances,
+        momentum,
+        price_volatility,
+        df["bid_vwap"].diff(liquidity_steps_forward), df["ask_vwap"].diff(liquidity_steps_forward)
+     ],
+     axis=1).dropna()
+
+liquidity_features.to_csv("data/processed/{}.csv".format(input_file_name))
